ee183f85e2bb8158d8ac439c85bb125290a10fb9
Josh <josh123dawkins@yahoo.com>
2023-05-08 20:28:01 -0400

    working search

--- public/js/search.js
+++ public/js/search.js
@@ -2,12 +2,13 @@ const searchResults = async (event) => {
     event.preventDefault();
     const plant = document.querySelector('#search').value.trim();
     document.location.replace(`/search?plant=${plant}`)
-    // fetch(`/search?plant=${plant}`,{
-    //     method: 'GET'
-    // })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
 }
 
+// const detailsPage = async (event) => {
+//     event.preventDefault();
+//     const id = document.querySelector('.picRes').id;
+//     document.location.replace(`/search/${id}`)
+// }
+
 document.querySelector('.searchBTN').addEventListener('click', searchResults);
+// document.querySelector('.picRes').addEventListener('click', detailsPage);
