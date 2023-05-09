6a624e298549416e8be7391baa448c97512f0be5
Josh <josh123dawkins@yahoo.com>
2023-05-09 17:10:46 -0400

    bug fixed

--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,8 @@
 const searchResults = async (event) => {
     event.preventDefault();
     const plant = document.querySelector('#search').value.trim();
-    document.location.replace('/search?plant=${plant}')
+    // console.log('plant in search.js: ', plant)
+    document.location.replace(`/search?plant=${plant}`)
 }
 
 document.querySelector('.searchBTN').addEventListener('click', searchResults);
